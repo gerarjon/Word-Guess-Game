b19da6660fc6ab380c73cc881895da1ab33765fe
gerarjon <gerarsuaverdez@gmail.com>
2019-08-05 15:10:30 -0700

    added game functions and game start

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@
 // User guesses word by key press
 // Game determines if letter is correct
 // If correct, word replaces blank
-// If wrong, body part gets added to hangman 
+// If wrong, letter gets put with other wrong leters
 
 
 // GLOBAL VARIABLES //  
@@ -20,7 +20,7 @@ var chosenWord = "";
 var lettersInChosenWord= [];
 
 // The number of blanks/underscores that are in the chosen word will be stored in this array 
-var blank = [];
+var blank = 0;
 
 // The correct letter and blanks will be held in this array 
 var rightLetter = [];
@@ -41,13 +41,13 @@ var wins = 0;
 // --------------------------------------------
 
 // All functions in the game will be stored in this object
+// the functions in this object are not being called yet 
 var game = {
 
     // Function that starts/resets the game 
     start: function() {
         // Resets the guesses back to 5
         guessesLeft = 5;
-        console.log(guessesLeft); //testing
 
         // Chooses random word from the word list based on the length of the word array 
         chosenWord = word[Math.floor(Math.random() * word.length)];
@@ -56,14 +56,17 @@ var game = {
         // Splits the chosen word into individual letters
         lettersInChosenWord = chosenWord.split("");
 
+        // Counts the number of letters in the word
+        blank = lettersInChosenWord.length;
+
         // RESETS the array of right letters at each round
         rightLetter =[];
         
         // RESETS the array of wrongs letters at each round
-        wrongLetters = [];
+        wrongLetter = [];
 
         // Generates blanks based on the length of the word
-        for (var i = 0; i < chosenWord.length; i++) {
+        for (var i = 0; i < blank; i++) {
             rightLetter.push('_');
         }
         console.log(rightLetter); //testing
@@ -77,6 +80,65 @@ var game = {
         // Clears the wrong guesses form the previous round
         document.getElementById("wrongGuesses").innerHTML = wrongLetter.join(" ");
     },
+
+    // Function that will compare the letters in the word to the letters that the player chooses
+    checkLetter: function(letter) {
+        // will be toggled based on if the the letter is found in the word
+        var letterInWord = false;
+
+        // Loops that will check if the letter chosen by the player is in the word
+        for (var i = 0; i < blank; i++) {
+            // if the letter exists, letterInWord will be changed to true
+            if (chosenWord[i] == letter) {
+                letterInWord = true;
+            }
+        };
+
+        // If the letter exists, find where in the word it exists
+        if (letterInWord) {
+            for (var i = 0; i < blank; i++) {
+                // Puts the letter into the rightLetter array with every instance that the letter is there
+                if (chosenWord[i] == letter) {
+                    // Sets the specific blank equal to the letter when there is a match 
+                    rightLetter[i] = letter;
+                }
+            }
+        } else {
+            // If wrong, pushes the wrong letter into the wrong letter array
+            wrongLetter.push(letter);
+            // substracts the number of guesses by 1
+            guessesLeft--;
+        };
+    },
+
+    // Function that will run after each guess is made
+    roundComplete: function() {
+        // logs how many wins and guesses are left
+        console.log(`Win(s): ${wins} | Guesses Left: ${guessesLeft}`); //testing
+
+        // Updates the HTML to reflect the # of guesses and replaces the blanks with the correct letter
+        document.getElementById("numOfGuesses").innerHTML = guessesLeft;
+        document.getElementById("wordBlanks").innerHTML = rightLetter.join(" ");
+        document.getElementById("wrongGuesses").innerHTML = wrongLetter.join(" ");
+
+        // If all the letters in the word are correct
+        if (lettersInChosenWord.toString() == rightLetter.toString()) {
+            wins++; // adds to wins counter
+            alert("You win!");
+
+            // Updates the # of wins in the HTML
+            document.getElementById("winCounter").innerHTML = wins;
+            
+            // Restarts the game
+            this.start();
+        } else if (guessesLeft == 0) {
+            // Alerts the player that they lose
+            alert("You're a big loser, idiot");
+
+            // restarts the game
+            this.start();
+        }
+    }
 };
 
 // // User's guess
@@ -109,6 +171,25 @@ var game = {
 //     console.log(userGuess);
 // };
 
+// Main Process
+// ---------------------------------------
+// Functions will be called/Game will start here 
+
+// Starts the game
+game.start();
+
+// Captures the player's key input
+document.onkeyup = function (event) {
+
+    // This variable takes the key entered and converts the unicode to a string letter and lowercases it
+    letterGuessed = String.fromCharChode(event.keyCode).toLowerCase();
+
+    // Checks if the letter is correct
+    game.checkLetter(letterGuessed);
+
+    // Completes the round
+    game.roundComplete();
+};
 
 
 
